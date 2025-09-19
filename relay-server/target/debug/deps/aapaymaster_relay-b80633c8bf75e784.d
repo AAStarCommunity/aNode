f5b1548eae48e1bf9a27fa4b2cfd1217fca0d056
jhfnetboy <jhfnetboy@gmail.com>
2025-09-19 16:32:02 +0700

    Add relay-server Rust project initialization

---
+++ relay-server/target/debug/deps/aapaymaster_relay-b80633c8bf75e784.d
@@ -0,0 +1,5 @@
+/Users/jason/Dev/aastar/aPaymaster/aPaymaster/relay-server/target/debug/deps/aapaymaster_relay-b80633c8bf75e784.d: src/main.rs
+
+/Users/jason/Dev/aastar/aPaymaster/aPaymaster/relay-server/target/debug/deps/libaapaymaster_relay-b80633c8bf75e784.rmeta: src/main.rs
+
+src/main.rs:
---
+++ relay-server/target/debug/deps/aapaymaster_relay-eb5b4c2ff15050cd.d
@@ -0,0 +1,5 @@
+/Users/jason/Dev/aastar/aPaymaster/aPaymaster/relay-server/target/debug/deps/aapaymaster_relay-eb5b4c2ff15050cd.d: src/main.rs
+
+/Users/jason/Dev/aastar/aPaymaster/aPaymaster/relay-server/target/debug/deps/aapaymaster_relay-eb5b4c2ff15050cd: src/main.rs
+
+src/main.rs:
