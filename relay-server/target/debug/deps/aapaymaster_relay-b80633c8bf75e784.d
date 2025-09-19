57d09e7b431f14da4e6b03e9e39e8f8e7495db9b
jhfnetboy <jhfnetboy@gmail.com>
2025-09-19 16:34:02 +0700

    Update .gitignore to exclude Rust build artifacts and clean up target directory from git

--- relay-server/target/debug/deps/aapaymaster_relay-b80633c8bf75e784.d
+++
@@ -1,5 +0,0 @@
-/Users/jason/Dev/aastar/aPaymaster/aPaymaster/relay-server/target/debug/deps/aapaymaster_relay-b80633c8bf75e784.d: src/main.rs
-
-/Users/jason/Dev/aastar/aPaymaster/aPaymaster/relay-server/target/debug/deps/libaapaymaster_relay-b80633c8bf75e784.rmeta: src/main.rs
-
-src/main.rs:
--- relay-server/target/debug/deps/aapaymaster_relay-eb5b4c2ff15050cd.d
+++
@@ -1,5 +0,0 @@
-/Users/jason/Dev/aastar/aPaymaster/aPaymaster/relay-server/target/debug/deps/aapaymaster_relay-eb5b4c2ff15050cd.d: src/main.rs
-
-/Users/jason/Dev/aastar/aPaymaster/aPaymaster/relay-server/target/debug/deps/aapaymaster_relay-eb5b4c2ff15050cd: src/main.rs
-
-src/main.rs:
